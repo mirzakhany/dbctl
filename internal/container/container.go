4c973deb587ad9406d25ed088d41797d2e59832b
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2022-09-13 19:24:53 +0200

    use docker sdk to manage containers

--- internal/container/container.go
+++ internal/container/container.go
@@ -3,16 +3,77 @@ package container
 import (
 	"context"
 
-	"github.com/testcontainers/testcontainers-go"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
-func Run(ctx context.Context, req testcontainers.ContainerRequest) (testcontainers.Container, error) {
-	cn, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
+type Request struct {
+	Name         string
+	Image        string
+	ExposedPorts []string // allow specifying protocol info
+	Cmd          []string
+	Env          map[string]string
+}
+
+type Container struct {
+	ID string
+}
+
+func Run(ctx context.Context, req Request) (*Container, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		return nil, err
 	}
+
+	reader, err := cli.ImagePull(ctx, req.Image, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		_ = reader.Close()
+	}()
+
+	env := []string{}
+	for envKey, envVar := range req.Env {
+		env = append(env, envKey+"="+envVar)
+	}
+
+	exposedPortSet, exposedPortMap, err := nat.ParsePortSpecs(req.ExposedPorts)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:        req.Image,
+		Cmd:          req.Cmd,
+		Env:          env,
+		ExposedPorts: exposedPortSet,
+	}, &container.HostConfig{PortBindings: exposedPortMap}, nil, nil, req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	cn := &Container{ID: resp.ID}
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return cn, err
+	}
+
 	return cn, nil
+}
+
+func (c *Container) Terminate(ctx context.Context) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return err
+	}
+
+	err = cli.ContainerRemove(ctx, c.ID, types.ContainerRemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         true,
+	})
+	return err
 }
--- internal/pg/pg.go
+++ internal/pg/pg.go
@@ -15,7 +15,6 @@ import (
 	embedpg "github.com/fergusstrange/embedded-postgres"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/jackc/pgx/v4"
-	"github.com/testcontainers/testcontainers-go/wait"
 
 	// golang migration postgres driver
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
@@ -23,7 +22,6 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/mirzakhany/dbctl/internal/container"
 	"github.com/mirzakhany/dbctl/internal/pkg"
-	"github.com/testcontainers/testcontainers-go"
 )
 
 type Postgres struct {
@@ -53,7 +51,7 @@ func (p *Postgres) Start() error {
 	log.Printf("Starting postgres version %s on port %d ...\n", p.cfg.version, p.cfg.port)
 
 	var err error
-	var closeFunc func() error
+	var closeFunc func(ctx context.Context) error
 
 	if p.cfg.useDockerEngine {
 		closeFunc, err = p.startUsingDocker(ctx)
@@ -80,10 +78,16 @@ func (p *Postgres) Start() error {
 
 	<-ctx.Done()
 	log.Println("Shutdown signal received, stopping database")
-	return closeFunc()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	return closeFunc(shutdownCtx)
 }
 
-func (p *Postgres) startUsingNative() (func() error, error) {
+func (p *Postgres) startUsingNative() (func(ctx context.Context) error, error) {
 	config := embedpg.DefaultConfig().
 		Locale("en_US.UTF-8").
 		Username(p.cfg.user).
@@ -94,24 +98,25 @@ func (p *Postgres) startUsingNative() (func() error, error) {
 		Logger(p.cfg.logger)
 
 	database := embedpg.NewDatabase(config)
-	if err := database.Start(); err != nil {
-		return func() error {
-			return database.Stop()
-		}, err
+	closeFunc := func(ctx context.Context) error {
+		return database.Stop()
 	}
 
-	return func() error {
-		return database.Stop()
-	}, nil
+	if err := database.Start(); err != nil {
+		return closeFunc, err
+	}
+
+	return closeFunc, nil
 }
 
-func (p *Postgres) startUsingDocker(ctx context.Context) (func() error, error) {
+func (p *Postgres) startUsingDocker(ctx context.Context) (func(ctx context.Context) error, error) {
 	var rnd, err = rand.Int(rand.Reader, big.NewInt(20))
 	if err != nil {
 		return nil, err
 	}
+
 	port := strconv.Itoa(int(p.cfg.port))
-	pg, err := container.Run(ctx, testcontainers.ContainerRequest{
+	pg, err := container.Run(ctx, container.Request{
 		Image: getPostGisImage(p.cfg.version),
 		Env: map[string]string{
 			"POSTGRES_PASSWORD": p.cfg.pass,
@@ -120,27 +125,22 @@ func (p *Postgres) startUsingDocker(ctx context.Context) (func() error, error) {
 		},
 		Cmd:          []string{"postgres", "-c", "fsync=off", "-c", "synchronous_commit=off", "-c", "full_page_writes=off"},
 		ExposedPorts: []string{fmt.Sprintf("%s:5432/tcp", port)},
-		WaitingFor:   wait.ForListeningPort("5432/tcp"),
 		Name:         fmt.Sprintf("dbctl_%d_%d", time.Now().Unix(), rnd.Uint64()),
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	closeFunc := func() error {
-		_ = pg.Terminate(ctx)
-		return nil
+	closeFunc := func(ctx context.Context) error {
+		return pg.Terminate(ctx)
 	}
-	return closeFunc, nil
+
+	return closeFunc, waitForPostgres(ctx, p.URI())
 }
 
 func (p *Postgres) URI() string {
 	host := net.JoinHostPort("localhost", strconv.Itoa(int(p.cfg.port)))
 	return (&url.URL{Scheme: "postgres", User: url.UserPassword(p.cfg.user, p.cfg.pass), Host: host, Path: p.cfg.name, RawQuery: "sslmode=disable"}).String()
-}
-
-func (p *Postgres) printURI() {
-	log.Printf("postgres database is running on: %q", p.URI())
 }
 
 func (p *Postgres) runMigrations() error {
@@ -178,6 +178,28 @@ func (p *Postgres) applyFixtures(ctx context.Context) error {
 
 		if _, err := conn.Exec(ctx, string(b)); err != nil {
 			return fmt.Errorf("applying fixture file (%s) failed: %w", f, err)
+		}
+	}
+	return nil
+}
+
+func waitForPostgres(ctx context.Context, url string) error {
+	log.Println("Wait for database to boot up")
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+
+	ctx, cancel := context.WithTimeout(ctx, 20*time.Second)
+	defer cancel()
+
+	for range ticker.C {
+		conn, err := pgx.Connect(ctx, url)
+		if err != nil {
+			if err == context.DeadlineExceeded {
+				return err
+			}
+		} else {
+			_ = conn.Close(context.Background())
+			return nil
 		}
 	}
 	return nil
